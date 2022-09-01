0ef98534e8a41a3edcf921897214c7c760bffe62
Андрей <rex.kudimov@mail.ru>
2022-09-01 13:53:28 +0300

    Update and rename NewsPortal/settings.py to Новостной портал/settings.py

--- Новостной портал/settings.py
+++ Новостной портал/settings.py
@@ -164,12 +164,12 @@ ACCOUNT_FORMS = {'signup': 'sign.models.CommonSignupForm'}
 
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
-EMAIL_HOST_USER = 'Qdim2002'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_USER = '*****'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
 EMAIL_HOST_PASSWORD = '******'  # пароль от почты
 EMAIL_USE_SSL = True  # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
 
-DEFAULT_FROM_EMAIL = 'Qdim2002@yandex.ru'
+DEFAULT_FROM_EMAIL = '******'
 
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
 
